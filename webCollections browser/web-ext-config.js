d9abd7514e5c5fab5116a871918066c232f201fd
ShaddyDC <shaddythefirst@gmail.com>
2021-01-21 16:20:05 +0100

    Add configs for eslint and web-ext

---
+++ webCollections browser/web-ext-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    run: {
+        watchIgnored: [
+            '**/node_modules/**',
+        ],
+        startUrl: [
+            'https://osu.ppy.sh',
+            'https://osu.ppy.sh/beatmapsets/39804#osu/129891',
+            'about:devtools-toolbox?type=extension&id=webcollections%40shaddy.dev',
+            'about:addons',
+        ],
+    },
+};
