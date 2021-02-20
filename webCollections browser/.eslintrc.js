62d8b65c95956e5a9e102a9f4ffcc34ed7ff5498
ShaddyDC <shaddythefirst@gmail.com>
2021-02-20 14:44:46 +0100

    Add eslint

--- webCollections browser/.eslintrc.js
+++ webCollections browser/.eslintrc.js
@@ -1,10 +1,26 @@
 module.exports = {
-    parserOptions: {
-        project: "./tsconfig.json",
-        ecmaVersion: 2019,
-        sourceType: "module",
+    "env": {
+        "browser": true,
+        "es6": true
     },
-    env: { browser: true, es6: true },
-    root: true,
-    extends: ["xtrict"],
+    "extends": [
+        'airbnb-typescript/base',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2019,
+        "project": "./tsconfig.json",
+        "sourceType": "module",
+        "tsconfigRootDir": __dirname
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "max-classes-per-file": 0,
+        "no-console": 0
+    },
+    "root": true
 };
